aeecf0d9314fcd1035a38c5943073f06a7b16f8f
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2017-06-11 22:21:16 -0300

    Some loop unrolling

--- vpx_dsp/ppc/fwd_txfm_vsx.c
+++ vpx_dsp/ppc/fwd_txfm_vsx.c
@@ -29,15 +29,14 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
     tran_high_t in_high[4];    // canbe16
     tran_high_t step[4];       // canbe16
     tran_high_t temp1, temp2;  // needs32
-    int i;
-    for (i = 0; i < 4; ++i) {
+    {
       // Load inputs.
       if (pass == 0) {
         in_high[0] = input[0 * stride] * 16;
         in_high[1] = input[1 * stride] * 16;
         in_high[2] = input[2 * stride] * 16;
         in_high[3] = input[3 * stride] * 16;
-        if (i == 0 && in_high[0]) {
+        if (in_high[0]) {
           ++in_high[0];
         }
       } else {
@@ -46,7 +45,6 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
         in_high[1] = in_low[1 * 4];
         in_high[2] = in_low[2 * 4];
         in_high[3] = in_low[3 * 4];
-        ++in_low;
       }
       // Transform.
       step[0] = in_high[0] + in_high[3];
@@ -62,8 +60,93 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
       out[1] = (tran_low_t)fdct_round_shift(temp1);
       out[3] = (tran_low_t)fdct_round_shift(temp2);
       // Do next column (which is a transposed row in second/horizontal pass)
-      ++input;
-      out += 4;
+    }
+    {
+      // Load inputs.
+      if (pass == 0) {
+        in_high[0] = (input[0 * stride + 1]) * 16;
+        in_high[1] = (input[1 * stride + 1]) * 16;
+        in_high[2] = (input[2 * stride + 1]) * 16;
+        in_high[3] = (input[3 * stride + 1]) * 16;
+      } else {
+        assert(in_low != NULL);
+        in_high[0] = in_low[0 * 4 + 1];
+        in_high[1] = in_low[1 * 4 + 1];
+        in_high[2] = in_low[2 * 4 + 1];
+        in_high[3] = in_low[3 * 4 + 1];
+      }
+      // Transform.
+      step[0] = in_high[0] + in_high[3];
+      step[1] = in_high[1] + in_high[2];
+      step[2] = in_high[1] - in_high[2];
+      step[3] = in_high[0] - in_high[3];
+      temp1 = (step[0] + step[1]) * cospi_16_64;
+      temp2 = (step[0] - step[1]) * cospi_16_64;
+      out[4] = (tran_low_t)fdct_round_shift(temp1);
+      out[6] = (tran_low_t)fdct_round_shift(temp2);
+      temp1 = step[2] * cospi_24_64 + step[3] * cospi_8_64;
+      temp2 = -step[2] * cospi_8_64 + step[3] * cospi_24_64;
+      out[5] = (tran_low_t)fdct_round_shift(temp1);
+      out[7] = (tran_low_t)fdct_round_shift(temp2);
+      // Do next column (which is a transposed row in second/horizontal pass)
+    }
+    {
+      // Load inputs.
+      if (pass == 0) {
+        in_high[0] = (input[0 * stride + 2]) * 16;
+        in_high[1] = (input[1 * stride + 2]) * 16;
+        in_high[2] = (input[2 * stride + 2]) * 16;
+        in_high[3] = (input[3 * stride + 2]) * 16;
+      } else {
+        assert(in_low != NULL);
+        in_high[0] = in_low[0 * 4 + 2];
+        in_high[1] = in_low[1 * 4 + 2];
+        in_high[2] = in_low[2 * 4 + 2];
+        in_high[3] = in_low[3 * 4 + 2];
+      }
+      // Transform.
+      step[0] = in_high[0] + in_high[3];
+      step[1] = in_high[1] + in_high[2];
+      step[2] = in_high[1] - in_high[2];
+      step[3] = in_high[0] - in_high[3];
+      temp1 = (step[0] + step[1]) * cospi_16_64;
+      temp2 = (step[0] - step[1]) * cospi_16_64;
+      out[8] = (tran_low_t)fdct_round_shift(temp1);
+      out[10] = (tran_low_t)fdct_round_shift(temp2);
+      temp1 = step[2] * cospi_24_64 + step[3] * cospi_8_64;
+      temp2 = -step[2] * cospi_8_64 + step[3] * cospi_24_64;
+      out[9] = (tran_low_t)fdct_round_shift(temp1);
+      out[11] = (tran_low_t)fdct_round_shift(temp2);
+      // Do next column (which is a transposed row in second/horizontal pass)
+    }
+    {
+      // Load inputs.
+      if (pass == 0) {
+        in_high[0] = (input[0 * stride + 3]) * 16;
+        in_high[1] = (input[1 * stride + 3]) * 16;
+        in_high[2] = (input[2 * stride + 3]) * 16;
+        in_high[3] = (input[3 * stride + 3]) * 16;
+      } else {
+        assert(in_low != NULL);
+        in_high[0] = in_low[0 * 4 + 3];
+        in_high[1] = in_low[1 * 4 + 3];
+        in_high[2] = in_low[2 * 4 + 3];
+        in_high[3] = in_low[3 * 4 + 3];
+      }
+      // Transform.
+      step[0] = in_high[0] + in_high[3];
+      step[1] = in_high[1] + in_high[2];
+      step[2] = in_high[1] - in_high[2];
+      step[3] = in_high[0] - in_high[3];
+      temp1 = (step[0] + step[1]) * cospi_16_64;
+      temp2 = (step[0] - step[1]) * cospi_16_64;
+      out[12] = (tran_low_t)fdct_round_shift(temp1);
+      out[14] = (tran_low_t)fdct_round_shift(temp2);
+      temp1 = step[2] * cospi_24_64 + step[3] * cospi_8_64;
+      temp2 = -step[2] * cospi_8_64 + step[3] * cospi_24_64;
+      out[13] = (tran_low_t)fdct_round_shift(temp1);
+      out[15] = (tran_low_t)fdct_round_shift(temp2);
+      // Do next column (which is a transposed row in second/horizontal pass)
     }
     // Setup in/out for next pass.
     in_low = intermediate;
@@ -71,9 +154,24 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
   }
 
   {
-    int i, j;
-    for (i = 0; i < 4; ++i) {
-      for (j = 0; j < 4; ++j) output[j + i * 4] = (output[j + i * 4] + 1) >> 2;
-    }
+    output[0 + 0 * 4] = (output[0 + 0 * 4] + 1) >> 2;
+    output[1 + 0 * 4] = (output[1 + 0 * 4] + 1) >> 2;
+    output[2 + 0 * 4] = (output[2 + 0 * 4] + 1) >> 2;
+    output[3 + 0 * 4] = (output[3 + 0 * 4] + 1) >> 2;
+
+    output[0 + 1 * 4] = (output[0 + 1 * 4] + 1) >> 2;
+    output[1 + 1 * 4] = (output[1 + 1 * 4] + 1) >> 2;
+    output[2 + 1 * 4] = (output[2 + 1 * 4] + 1) >> 2;
+    output[3 + 1 * 4] = (output[3 + 1 * 4] + 1) >> 2;
+
+    output[0 + 2 * 4] = (output[0 + 2 * 4] + 1) >> 2;
+    output[1 + 2 * 4] = (output[1 + 2 * 4] + 1) >> 2;
+    output[2 + 2 * 4] = (output[2 + 2 * 4] + 1) >> 2;
+    output[3 + 2 * 4] = (output[3 + 2 * 4] + 1) >> 2;
+
+    output[0 + 3 * 4] = (output[0 + 3 * 4] + 1) >> 2;
+    output[1 + 3 * 4] = (output[1 + 3 * 4] + 1) >> 2;
+    output[2 + 3 * 4] = (output[2 + 3 * 4] + 1) >> 2;
+    output[3 + 3 * 4] = (output[3 + 3 * 4] + 1) >> 2;
   }
 }
