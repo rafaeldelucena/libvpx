8155ae048a96488883834b68f07bd083558214ff
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2017-06-29 22:53:49 -0300

    Remove inter pass variables

--- vpx_dsp/ppc/fwd_txfm_vsx.c
+++ vpx_dsp/ppc/fwd_txfm_vsx.c
@@ -22,8 +22,6 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
   // in normal/row positions).
   // We need an intermediate buffer between passes.
   tran_low_t intermediate[16];
-  const tran_low_t *in_low = NULL;
-  tran_low_t *out = intermediate;
   // Do the two transform/transpose passes
   {
     tran_high_t in_high[16];    // canbe16
@@ -99,31 +97,28 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
       temp[14] = step[14] * cospi_24_64 + step[15] * cospi_8_64;
       temp[15] = -step[14] * cospi_8_64 + step[15] * cospi_24_64;
 
-      out[0] = (tran_low_t)fdct_round_shift(temp[0]);
-      out[1] = (tran_low_t)fdct_round_shift(temp[2]);
-      out[2] = (tran_low_t)fdct_round_shift(temp[1]);
-      out[3] = (tran_low_t)fdct_round_shift(temp[3]);
+      intermediate[0] = (tran_low_t)fdct_round_shift(temp[0]);
+      intermediate[1] = (tran_low_t)fdct_round_shift(temp[2]);
+      intermediate[2] = (tran_low_t)fdct_round_shift(temp[1]);
+      intermediate[3] = (tran_low_t)fdct_round_shift(temp[3]);
 
-      out[4] = (tran_low_t)fdct_round_shift(temp[4]);
-      out[5] = (tran_low_t)fdct_round_shift(temp[6]);
-      out[6] = (tran_low_t)fdct_round_shift(temp[5]);
-      out[7] = (tran_low_t)fdct_round_shift(temp[7]);
+      intermediate[4] = (tran_low_t)fdct_round_shift(temp[4]);
+      intermediate[5] = (tran_low_t)fdct_round_shift(temp[6]);
+      intermediate[6] = (tran_low_t)fdct_round_shift(temp[5]);
+      intermediate[7] = (tran_low_t)fdct_round_shift(temp[7]);
 
-      out[8] = (tran_low_t)fdct_round_shift(temp[8]);
-      out[9] = (tran_low_t)fdct_round_shift(temp[10]);
-      out[10] = (tran_low_t)fdct_round_shift(temp[9]);
-      out[11] = (tran_low_t)fdct_round_shift(temp[11]);
+      intermediate[8] = (tran_low_t)fdct_round_shift(temp[8]);
+      intermediate[9] = (tran_low_t)fdct_round_shift(temp[10]);
+      intermediate[10] = (tran_low_t)fdct_round_shift(temp[9]);
+      intermediate[11] = (tran_low_t)fdct_round_shift(temp[11]);
 
-      out[12] = (tran_low_t)fdct_round_shift(temp[12]);
-      out[13] = (tran_low_t)fdct_round_shift(temp[14]);
-      out[14] = (tran_low_t)fdct_round_shift(temp[13]);
-      out[15] = (tran_low_t)fdct_round_shift(temp[15]);
+      intermediate[12] = (tran_low_t)fdct_round_shift(temp[12]);
+      intermediate[13] = (tran_low_t)fdct_round_shift(temp[14]);
+      intermediate[14] = (tran_low_t)fdct_round_shift(temp[13]);
+      intermediate[15] = (tran_low_t)fdct_round_shift(temp[15]);
 
       // Do next column (which is a transposed row in second/horizontal pass)
     }
-    // Setup in/out for next pass.
-    in_low = intermediate;
-    out = output;
   }
   {
     tran_high_t in_high[16];    // canbe16
@@ -136,24 +131,24 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
       // Load inputs.
       {
         // in_high1
-        in_high[0] = in_low[0];
-        in_high[4] = in_low[1];
-        in_high[8] = in_low[2];
-        in_high[12]= in_low[3];
-        in_high[1] = in_low[4];
-        in_high[5] = in_low[5];
-        in_high[9] = in_low[6];
-        in_high[13]= in_low[7];
+        in_high[0] = intermediate[0];
+        in_high[4] = intermediate[1];
+        in_high[8] = intermediate[2];
+        in_high[12]= intermediate[3];
+        in_high[1] = intermediate[4];
+        in_high[5] = intermediate[5];
+        in_high[9] = intermediate[6];
+        in_high[13]= intermediate[7];
 
         // in_high2
-        in_high[2] = in_low[8];
-        in_high[6] = in_low[9];
-        in_high[10]= in_low[10];
-        in_high[14]= in_low[11];
-        in_high[3] = in_low[12];
-        in_high[7] = in_low[13];
-        in_high[11]= in_low[14];
-        in_high[15]= in_low[15];
+        in_high[2] = intermediate[8];
+        in_high[6] = intermediate[9];
+        in_high[10]= intermediate[10];
+        in_high[14]= intermediate[11];
+        in_high[3] = intermediate[12];
+        in_high[7] = intermediate[13];
+        in_high[11]= intermediate[14];
+        in_high[15]= intermediate[15];
       }
       // Transform.
       //step1 = vec_add(in_high1, in_high2);
@@ -197,25 +192,25 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
       temp[14] = step[14] * cospi_24_64 + step[15] * cospi_8_64;
       temp[15] = -step[14] * cospi_8_64 + step[15] * cospi_24_64;
 
-      out[0] = (tran_low_t)fdct_round_shift(temp[0]);
-      out[1] = (tran_low_t)fdct_round_shift(temp[2]);
-      out[2] = (tran_low_t)fdct_round_shift(temp[1]);
-      out[3] = (tran_low_t)fdct_round_shift(temp[3]);
+      output[0] = (tran_low_t)fdct_round_shift(temp[0]);
+      output[1] = (tran_low_t)fdct_round_shift(temp[2]);
+      output[2] = (tran_low_t)fdct_round_shift(temp[1]);
+      output[3] = (tran_low_t)fdct_round_shift(temp[3]);
 
-      out[4] = (tran_low_t)fdct_round_shift(temp[4]);
-      out[5] = (tran_low_t)fdct_round_shift(temp[6]);
-      out[6] = (tran_low_t)fdct_round_shift(temp[5]);
-      out[7] = (tran_low_t)fdct_round_shift(temp[7]);
+      output[4] = (tran_low_t)fdct_round_shift(temp[4]);
+      output[5] = (tran_low_t)fdct_round_shift(temp[6]);
+      output[6] = (tran_low_t)fdct_round_shift(temp[5]);
+      output[7] = (tran_low_t)fdct_round_shift(temp[7]);
 
-      out[8] = (tran_low_t)fdct_round_shift(temp[8]);
-      out[9] = (tran_low_t)fdct_round_shift(temp[10]);
-      out[10] = (tran_low_t)fdct_round_shift(temp[9]);
-      out[11] = (tran_low_t)fdct_round_shift(temp[11]);
+      output[8] = (tran_low_t)fdct_round_shift(temp[8]);
+      output[9] = (tran_low_t)fdct_round_shift(temp[10]);
+      output[10] = (tran_low_t)fdct_round_shift(temp[9]);
+      output[11] = (tran_low_t)fdct_round_shift(temp[11]);
 
-      out[12] = (tran_low_t)fdct_round_shift(temp[12]);
-      out[13] = (tran_low_t)fdct_round_shift(temp[14]);
-      out[14] = (tran_low_t)fdct_round_shift(temp[13]);
-      out[15] = (tran_low_t)fdct_round_shift(temp[15]);
+      output[12] = (tran_low_t)fdct_round_shift(temp[12]);
+      output[13] = (tran_low_t)fdct_round_shift(temp[14]);
+      output[14] = (tran_low_t)fdct_round_shift(temp[13]);
+      output[15] = (tran_low_t)fdct_round_shift(temp[15]);
 
       // Do next column (which is a transposed row in second/horizontal pass)
     }
